05181d5e6a696b9328fed2f26444a805117edb28
Sujit <xsujit@gmail.com>
2020-05-28 23:24:08 +0100

    Using redirected url during login

--- src/test/locustfile.py
+++ src/test/locustfile.py
@@ -1,66 +1,50 @@
 from locust import HttpLocust, TaskSet, task, between
-import certifi
-import urllib3
-import ssl
+import requests
 
-def cert_exists():
-    with open(certifi.where()) as cr_file:
-        if ssl.get_server_certificate(('localhost', 8443)) in cr_file.read():
-            print("Cert exists in file")
-            return True
-        else:
-            return False
-            
-
-if(cert_exists() != True):
-    cert_file = open(certifi.where(), "a")        
-    cert_file.write(ssl.get_server_certificate(('localhost', 8443)))
-    cert_file.close()
-        
 
 class UserBehavior(TaskSet):
 
     def on_start(self):
         self.login()
 
-
     def on_stop(self):
         self.logout()
 
-        
     def login(self):
-        self.client.post("/login", {"username":"jack.bauer@mastek.com", "password":"password"}, verify=certifi.where())
-
+        r = requests.get("http://localhost:8080/appraisal", allow_redirects=False)
+        redirect_url = r.headers['Location']
+        url = redirect_url[redirect_url.rfind("/"):]
+        r = self.client.post(url, {"username": "jack.bauer@mastek.com", "password": "password"})
+        print(r.content)
 
     def logout(self):
-        self.client.post("/logout", {"username":"jack.bauer@mastek.com", "password":"password"}, verify=certifi.where())
-
+        self.client.post("/logout", {"username": "jack.bauer@mastek.com", "password": "password"})
 
     @task(1)
     def index(self):
-        self.client.get("/", verify=certifi.where())
-
+        self.client.get("/")
 
     @task(2)
     def profile(self):
-        r = self.client.get("/appraisal", verify=certifi.where())
+        r = self.client.get("/appraisal")
         assert r.status_code == 200 and "Project Title" in r.text
 
-        
     @task(2)
     def team(self):
-        r = self.client.get("/team", verify=certifi.where(), headers={"content-type":"application/json"})        
+        r = self.client.get("/team", headers={"content-type": "application/json"})
         assert r.status_code == 200 and "Here you can view all your team members" in r.text
 
 
 class WebsiteUser(HttpLocust):
 
-    def setup(self):
+    @staticmethod
+    def setup():
         print("setup called")
-    
-    def teardown(self):
+
+    @staticmethod
+    def teardown():
         print("teardown  called")
-    
-    host = "https://localhost:8443"
+
+    host = "http://localhost:8080"
     task_set = UserBehavior
-    wait_time = between(5.0, 9.0)
+    wait_time = between(10, 10)
